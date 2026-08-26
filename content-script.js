0a065b4411812a1e61e4f7e3b0cbd9853a49014b
armand2j <armandojfa12@gmail.com>
2026-08-25 23:41:33 -0400

    ui redesign

--- content-script.js
+++ content-script.js
@@ -49,7 +49,59 @@ chrome.runtime.onMessage.addListener((request, sender) => {
 				const summarizeBtn = document.createElement('button');
 				summarizeBtn.textContent = 'Summarize';
 				summarizeBtn.id = summarizeBtnId;
-				summarizeBtn.style = style();
+				const styleElm = document.createElement('style');
+				styleElm.textContent = `
+					:root {
+						--text-color: rgba(27, 27, 37, 0.8);
+						--text-color-disabled: rgba(255, 0, 0, 0.97);
+						--bg-color: rgb(211 227 253);
+						--bg-color-hover: rgb(194 211 237);
+						--bg-color-active: rgb(175 193 219);
+						--bg-color-disabled: rgb(229 229 229);
+					}
+
+					#summarize-btn {
+						font-family: inherit;
+						font-size: 0.9rem;
+						border: none;
+						color: var(--text-color);
+						font-weight: 500;
+						padding: 10px 20px;
+						background-color: var(--bg-color);
+						border-radius: 20px;
+						cursor: pointer;
+						z-index: 5;
+						margin-left: 10px;
+					}
+
+					#summarize-btn:hover {
+						background-color: var(--bg-color-hover)
+					}
+
+					#summarize-btn:active {
+						background-color: var(--bg-color-active)
+					}
+
+					#summarize-btn:disabled {
+						background-color: var(--bg-color-disabled);
+						color: var(--text-color-disabled)
+						cursor: normal;
+					}
+					
+					${summarizeBtnContainerSelector} {
+						display: flex;
+						flexd-direction: row;
+						align-items: center;
+					}
+				`;
+
+				summarizeBtn.addEventListener('click', (ev) => {
+					console.log(document.querySelector('.a3s').innerText);
+					summarizeBtn.setAttribute('disabled', '');
+					console.log('dag');
+				});
+
+				container.appendChild(styleElm);
 				container.appendChild(summarizeBtn);
 			});
 		}
